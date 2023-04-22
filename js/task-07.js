fd740c574979578f77458de469cda93fb5074dd3
Oleksandr Tymoshenko <hermeusmanua@gmail.com>
2023-04-22 13:25:42 +0300

    task-07 fix bugs

--- js/task-07.js
+++ js/task-07.js
@@ -6,7 +6,7 @@
 
 const input = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
-
+text.style.fontSize = `${input.value}px`;
 input.addEventListener("input", (event) => {
   text.style.fontSize = `${event.currentTarget.value}px`;
 });
