29b336b3d5fd150a90773485ee3c941655bb6a7f
Oleksandr Tymoshenko <hermeusmanua@gmail.com>
2023-04-22 12:15:45 +0300

    task-01, task-02 fix bugs

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,12 @@
 // Напиши скрипт, який:
 
-// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
-// Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
+// Порахує і виведе в консоль кількість категорій в ul#categories,
+//  тобто елементів li.item.
+// Для кожного элемента li.item у списку ul#categories, знайде
+// і виведе в консоль текст заголовку елемента(тегу < h2 >) і
+// кількість елементів в категорії(усіх < li >, вкладених в нього).
+// Для виконання цього завдання потрібно використати метод
+// forEach() і властивості навігації по DOM.
 
 // В результаті, в консолі будуть виведені наступні повідомлення:
 // Number of categories: 3
@@ -13,11 +17,23 @@
 // Category: Technologies
 // Elements: 5
 
-const categories = document.querySelector("#categories").children;
+const categories = document.querySelectorAll("#categories > li.item");
 const numberOfCategories = categories.length;
 console.log("  Number of categories: " + numberOfCategories);
-const categoriesArray = Array.from(categories);
-categoriesArray.forEach((elem) => {
-  console.log(`  Category: ${elem.querySelector("h2").textContent}
-  Elements: ${elem.querySelector("ul").children.length}`);
+categories.forEach((elem) => {
+  console.log(`  Category: ${elem.firstElementChild.textContent}
+  Elements: ${elem.lastElementChild.children.length}`);
+  //! Також можна: console.log(`  Category: ${elem.lastElementChild.previousElementSibling.textContent}
+  //! Також можна:  Elements: ${elem.firstElementChild.nextElementSibling.children.length}`);
+  //! Et cetera, und so weiter, тощо...
 });
+
+//! PREVIOUS VERSION
+//? const categories = document.querySelector("#categories").children;
+//? const numberOfCategories = categories.length;
+//? console.log("  Number of categories: " + numberOfCategories);
+//? const categoriesArray = Array.from(categories);
+//? categoriesArray.forEach((elem) => {
+//?   console.log(`  Category: ${elem.querySelector("h2").textContent}
+//?   Elements: ${elem.querySelector("ul").children.length}`);
+//? });
--- js/task-02.js
+++ js/task-02.js
@@ -1,21 +1,10 @@
-// HTML містить порожній список ul#ingredients.
-// <ul id="ingredients"></ul>
-
-// JavaScript містить масив рядків:
-// const ingredients = [
-//   "Potatoes",
-//   "Mushrooms",
-//   "Garlic",
-//   "Tomatos",
-//   "Herbs",
-//   "Condiments",
-// ];
-
 // Напиши скрипт, який для кожного елемента масиву ingredients:
-// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Створить окремий елемент <li>. Обов'язково використовуй метод
+// document.createElement().
 // Додасть назву інгредієнта як його текстовий вміст.
 // Додасть елементу клас item.
-// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+// Після чого, вставить усі <li> за одну операцію у список
+// ul#ingredients.
 
 const ingredients = [
   "Potatoes",
@@ -28,22 +17,22 @@ const ingredients = [
 
 const ingredientsList = document.querySelector("#ingredients");
 
-const list = [];
-
-ingredients.forEach((elem) => {
+const list = ingredients.map((elem) => {
   const listItem = document.createElement("li");
-
-  listItem.textContent = elem;
-
   listItem.classList.add("item");
-
-  // console.log(listItem);
-
-  list.push(listItem);
+  listItem.textContent = elem;
+  return listItem;
 });
 
-// console.log(list);
-
 ingredientsList.append(...list);
-
 console.log(ingredientsList);
+
+//! PREVIOUS VERSION
+// ingredients.forEach((elem) => {
+//   const listItem = document.createElement("li");
+//   listItem.textContent = elem;
+//   listItem.classList.add("item");
+//   console.log(listItem);
+//   list.push(listItem);
+// });
+// console.log(list);
