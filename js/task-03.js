b43ff0130f5d293a3ae7485e209290f531e7ab36
Oleksandr Tymoshenko <hermeusmanua@gmail.com>
2023-04-22 12:38:14 +0300

    task-03 fix bugs

--- js/task-03.js
+++ js/task-03.js
@@ -1,23 +1,13 @@
-// Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
-// <ul class="gallery"></ul>
+// Напиши скрипт для створення галереї зображень на підставі
+// масиву даних.HTML містить список ul.gallery.
 
-// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
-// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
-// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
-// const images = [
-//   {
-//     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-//     alt: "White and Black Long Fur Cat",
-//   },
-//   {
-//     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-//   },
-//   {
-//     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-//     alt: "Group of Horses Running",
-//   },
-// ];
+// Використовуй масив об'єктів images для створення елементів
+// <img>, вкладених в <li>. Для створення розмітки використовуй
+// шаблонні рядки і метод insertAdjacentHTML(). * /}
+// Усі елементи галереї повинні додаватися в DOM за одну операцію
+// додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами
+// через CSS класи.
 
 const images = [
   {
@@ -34,11 +24,21 @@ const images = [
   },
 ];
 
-let list = "";
-images.forEach((img) => {
-  list += `<li><img src="${img.url}" alt="${img.alt}"/></li>`;
-});
-// console.log(list);
 const galleryList = document.querySelector("ul.gallery");
+
+let list = images.reduce((previousValue, elem) => {
+  previousValue += `<li><img src="${elem.url}" alt="${elem.alt}"/></li>`;
+  return previousValue;
+}, "");
 galleryList.insertAdjacentHTML("afterbegin", list);
 console.log(galleryList);
+// console.log(list);
+
+//! PREVIOUS VERSION
+// images.forEach((img) => {
+//   list += `<li><img src="${img.url}" alt="${img.alt}"/></li>`;
+// });
+// console.log(list);
+// const galleryList = document.querySelector("ul.gallery");
+// galleryList.insertAdjacentHTML("afterbegin", list);
+// console.log(galleryList);
